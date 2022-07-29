5b5aa59c954c99d80a83553f3645dd4107e518ef
dvdapogeo <dvdingles@hotmail.com>
2022-07-29 12:30:14 +0200

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,31 @@
+import { OrbitControls } from '@react-three/drei'
+import { Canvas } from '@react-three/fiber'
+import type { NextPage } from 'next'
+import { Suspense } from 'react'
+import Solo from '../components/Solo'
+
+
+
+const Home: NextPage = () => {
+  return (
+    <div style={{width: '555', height: '777px', border: 'solid 2px #eee', backgroundColor: '#f5f5f5'}}>
+    <Canvas camera={{zoom: 2, position:[1, .5 , -1]}} >
+      {/* <focus-camera> */}
+      <ambientLight intensity={.5} />
+      <pointLight position={[10, 10, 10]} />
+      <Suspense fallback={null}>
+        <Solo 
+          position={[0,0,0]}
+          rotation={[0,1.5,0]}
+          />
+      </Suspense>
+      <OrbitControls />
+    </Canvas>
+  </div>
+
+  )
+}
+
+
+
+export default Home
